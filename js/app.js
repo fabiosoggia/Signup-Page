0bfb40c0e97dac1a4b1e31ffa66a2e3129918b91
Fabio Soggia <f.soggia@gmail.com>
2015-11-16 22:16:56 +0100

    Better form validation.

--- js/app.js
+++ js/app.js
@@ -2,16 +2,18 @@
 
 $(function DOMReady() {
 
-	// window.ParsleyValidator.addValidator("alpha", {
-	// 	pattern: new RegExp("[A-Za-z ]+"),
-	// 	validateNumber: function(value) {
-	// 		value = value.trim();
-	// 		return this.pattern.test(value);
-	// 	},
-	// 	messages: {
-	// 		en: "This field must contain only letters and spaces"
-	// 	}
-	// });
+	window.ParsleyValidator.addValidator("alpha", {
+		requirementType: "boolean",
+		pattern: /^([A-Za-z ]+)$/,
+		validateString: function(value) {
+			value = value.trim();
+			return this.pattern.test(value);
+		},
+		messages: {
+			en: "This field must contain only letters and spaces."
+		}
+	});
+
 
 	$("#signin-form").parsley();
 });
